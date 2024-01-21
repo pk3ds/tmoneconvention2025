60f197ed198fd1518f2ad61491060b99e8923cba
nazrinputra <putra_naz94@yahoo.com>
2024-01-21 12:09:29 +0800

    Install tsparticles

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,8 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import Particles from "@tsparticles/vue3";
+import { loadFull } from "tsparticles";
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -16,6 +18,11 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(Particles, {
+                init: async engine => {
+                    await loadFull(engine);
+                },
+            })
             .mount(el);
     },
     progress: {
