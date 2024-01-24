2ab9afc6c31950389a8556d37ef7a93b5bf41a4d
nazrinputra <putra_naz94@yahoo.com>
2024-01-24 14:57:28 +0800

    Add day one and day two

--- resources/js/Pages/Welcome/Partials/Agenda.vue
+++ resources/js/Pages/Welcome/Partials/Agenda.vue
@@ -1,5 +1,5 @@
 <script setup>
-const items = [
+const dayOne = [
     {
         id: 1,
         question: "11.00 AM",
@@ -10,23 +10,71 @@ const items = [
         question: "11.30 AM",
         answer: "<b>Registration & Arrival of guests<br/></b>Amar Huzaimi Md Deris, Group Chief Executive Officer, TM ",
     },
-    // More items...
+    // More dayOne...
+];
+
+const dayTwo = [
+    {
+        id: 1,
+        question: "10.00 AM",
+        answer: "<b>Registration & Arrival of guests<br/>Refreshments</b>",
+    },
+    {
+        id: 2,
+        question: "10.30 AM",
+        answer: "<b>Registration & Arrival of guests<br/></b>Amar Huzaimi Md Deris, Group Chief Executive Officer, TM ",
+    },
+    // More dayTwo...
 ];
 </script>
 
 <template>
-    <div class="bg-white">
-        <div
-            class="max-w-4xl px-6 py-24 mx-auto divide-y divide-gray-900/10 sm:py-32 lg:px-8 lg:py-40"
+    <div class="pt-24 bg-white">
+        <h2
+            class="text-3xl font-bold tracking-tight text-center uppercase mb-14 text-cobalt-blue font-title sm:text-4xl"
         >
-            <h2
-                class="text-3xl font-bold tracking-tight text-center uppercase text-cobalt-blue font-title sm:text-4xl"
-            >
-                Agenda
-            </h2>
-            <dl class="mt-10 space-y-8 divide-y divide-gray-900/10">
+            Agenda
+        </h2>
+        <p
+            class="mt-2 text-lg leading-8 text-center uppercase text-ultramarine font-title"
+        >
+            Day 1 - 29<sup>th</sup> January 2024
+        </p>
+        <div
+            class="max-w-4xl px-6 pb-24 mx-auto divide-y divide-gray-900/10 sm:pb-32 lg:px-8 lg:pb-40"
+        >
+            <dl class="space-y-8 divide-y divide-gray-900/10">
                 <div
-                    v-for="item in items"
+                    v-for="item in dayOne"
+                    :key="item.id"
+                    class="pt-8 lg:grid lg:grid-cols-6 lg:gap-8"
+                >
+                    <dt
+                        class="text-2xl font-bold leading-7 text-ultramarine lg:col-span-1"
+                    >
+                        {{ item.question }}
+                    </dt>
+                    <dd class="mt-4 lg:col-span-5 lg:mt-0">
+                        <p
+                            class="text-2xl leading-7 text-gray-900"
+                            v-html="item.answer"
+                        ></p>
+                    </dd>
+                </div>
+            </dl>
+        </div>
+
+        <p
+            class="mt-2 text-lg leading-8 text-center uppercase text-ultramarine font-title"
+        >
+            Day 2 - 30<sup>th</sup> January 2024
+        </p>
+        <div
+            class="max-w-4xl px-6 pb-24 mx-auto divide-y divide-gray-900/10 sm:pb-32 lg:px-8 lg:pb-40"
+        >
+            <dl class="space-y-8 divide-y divide-gray-900/10">
+                <div
+                    v-for="item in dayTwo"
                     :key="item.id"
                     class="pt-8 lg:grid lg:grid-cols-6 lg:gap-8"
                 >
