d880261683f561e372a9be2ba1e3e419f5f7ad85
nazrinputra <putra_naz94@yahoo.com>
2024-01-24 14:34:46 +0800

    Enable agenda

--- resources/js/Pages/Welcome/Index.vue
+++ resources/js/Pages/Welcome/Index.vue
@@ -3,8 +3,6 @@ import SiteLayout from "@/Layouts/SiteLayout.vue";
 import Agenda from "./Partials/Agenda.vue";
 import Hero from "./Partials/Hero.vue";
 import Countdown from "./Partials/Countdown.vue";
-import DayOne from "./Partials/DayOne.vue";
-import DayTwo from "./Partials/DayTwo.vue";
 import Attire from "./Partials/Attire.vue";
 import { Head } from "@inertiajs/vue3";
 </script>
@@ -20,17 +18,9 @@ import { Head } from "@inertiajs/vue3";
             <Countdown />
         </div>
 
-        <!-- <div class="relative z-10" id="agenda">
+        <div class="relative z-10" id="agenda">
             <Agenda />
-        </div> -->
-
-        <!-- <div class="relative z-10 py-12" id="agenda">
-            <DayOne />
         </div>
-
-        <div class="relative z-10 pb-12">
-            <DayTwo />
-        </div> -->
 
         <div class="relative z-10 py-12" id="dress-code">
             <Attire />
--- resources/js/Pages/Welcome/Partials/Agenda.vue
+++ resources/js/Pages/Welcome/Partials/Agenda.vue
@@ -1,39 +1,45 @@
 <script setup>
-const faqs = [
+const items = [
     {
         id: 1,
-        question: "What's the best thing about Switzerland?",
-        answer: "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+        question: "11.00 AM",
+        answer: "<b>Registration & Arrival of guests<br/>Refreshments</b>",
     },
-    // More questions...
+    {
+        id: 2,
+        question: "11.30 AM",
+        answer: "<b>Registration & Arrival of guests<br/></b>Amar Huzaimi Md Deris, Group Chief Executive Officer, TM ",
+    },
+    // More items...
 ];
 </script>
 
 <template>
     <div class="bg-white">
         <div
-            class="px-6 py-24 mx-auto divide-y max-w-7xl divide-gray-900/10 sm:py-32 lg:px-8 lg:py-40"
+            class="max-w-4xl px-6 py-24 mx-auto divide-y divide-gray-900/10 sm:py-32 lg:px-8 lg:py-40"
         >
             <h2
-                class="text-2xl font-bold leading-10 tracking-tight text-gray-900"
+                class="text-3xl font-bold tracking-tight text-center uppercase text-cobalt-blue font-title sm:text-4xl"
             >
-                Frequently asked questions
+                Agenda
             </h2>
             <dl class="mt-10 space-y-8 divide-y divide-gray-900/10">
                 <div
-                    v-for="faq in faqs"
-                    :key="faq.id"
-                    class="pt-8 lg:grid lg:grid-cols-12 lg:gap-8"
+                    v-for="item in items"
+                    :key="item.id"
+                    class="pt-8 lg:grid lg:grid-cols-6 lg:gap-8"
                 >
                     <dt
-                        class="text-base font-semibold leading-7 text-gray-900 lg:col-span-5"
+                        class="text-2xl font-bold leading-7 text-ultramarine lg:col-span-1"
                     >
-                        {{ faq.question }}
+                        {{ item.question }}
                     </dt>
-                    <dd class="mt-4 lg:col-span-7 lg:mt-0">
-                        <p class="text-base leading-7 text-gray-600">
-                            {{ faq.answer }}
-                        </p>
+                    <dd class="mt-4 lg:col-span-5 lg:mt-0">
+                        <p
+                            class="text-2xl leading-7 text-gray-900"
+                            v-html="item.answer"
+                        ></p>
                     </dd>
                 </div>
             </dl>
