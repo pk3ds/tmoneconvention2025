8b530a9a09ede6211eda2d97345df6959c080b5c
nazrinputra <putra_naz94@yahoo.com>
2024-01-26 11:40:32 +0800

    Remove unused code

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,13 +1,6 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { Head } from "@inertiajs/vue3";
-
-const stats = [
-    { id: 1, name: "Passport stamps", value: "8" },
-    { id: 2, name: "Points collected", value: "12,345" },
-    { id: 3, name: "Group name", value: "RedBull" },
-    { id: 4, name: "Room Number", value: "13-03" },
-];
 </script>
 
 <template>
@@ -41,34 +34,6 @@ const stats = [
                         class="absolute inset-0 object-cover w-full h-full -z-10"
                     />
                     <div class="relative px-6 mx-auto max-w-7xl lg:px-8">
-                        <div
-                            class="absolute -bottom-8 -left-96 -z-10 transform-gpu blur-3xl sm:-bottom-64 sm:-left-40 lg:-bottom-32 lg:left-8 xl:-left-10"
-                            aria-hidden="true"
-                        >
-                            <div
-                                class="aspect-[1266/975] w-[79.125rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-                                style="
-                                    clip-path: polygon(
-                                        74.1% 44.1%,
-                                        100% 61.6%,
-                                        97.5% 26.9%,
-                                        85.5% 0.1%,
-                                        80.7% 2%,
-                                        72.5% 32.5%,
-                                        60.2% 62.4%,
-                                        52.4% 68.1%,
-                                        47.5% 58.3%,
-                                        45.2% 34.5%,
-                                        27.5% 76.7%,
-                                        0.1% 64.9%,
-                                        17.9% 100%,
-                                        27.6% 76.8%,
-                                        76.1% 97.7%,
-                                        74.1% 44.1%
-                                    );
-                                "
-                            />
-                        </div>
                         <div class="max-w-2xl mx-auto lg:mx-0 lg:max-w-xl">
                             <h2
                                 class="text-base font-semibold leading-8 text-accent-orange"
