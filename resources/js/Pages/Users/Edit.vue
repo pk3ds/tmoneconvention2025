fece8a3ba86227a727d3cd48bc06aa0bd942de32
nazrinputra <putra_naz94@yahoo.com>
2024-01-26 08:44:21 +0800

    Remove users tab from user

--- resources/js/Pages/Users/Edit.vue
+++ resources/js/Pages/Users/Edit.vue
@@ -81,19 +81,6 @@ const pointsForm = useForm({
                             <li class="me-2" role="presentation">
                                 <button
                                     class="inline-block p-4 capitalize border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
-                                    id="users-tab"
-                                    data-tabs-target="#users"
-                                    type="button"
-                                    role="tab"
-                                    aria-controls="users"
-                                    aria-selected="false"
-                                >
-                                    users
-                                </button>
-                            </li>
-                            <li class="me-2" role="presentation">
-                                <button
-                                    class="inline-block p-4 capitalize border-b-2 rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300"
                                     id="awards-tab"
                                     data-tabs-target="#awards"
                                     type="button"
@@ -309,23 +296,6 @@ const pointsForm = useForm({
                                     </PrimaryButton>
                                 </div>
                             </form>
-                        </div>
-                        <div
-                            class="hidden p-4 rounded-lg"
-                            id="users"
-                            role="tabpanel"
-                            aria-labelledby="users-tab"
-                        >
-                            <p class="text-sm text-gray-500 dark:text-gray-400">
-                                This is some placeholder content the
-                                <strong
-                                    class="font-medium text-gray-800 dark:text-white"
-                                    >users tab's associated content</strong
-                                >. Clicking another tab will toggle the
-                                visibility of this one for the next. The tab
-                                JavaScript swaps classes to control the content
-                                visibility and styling.
-                            </p>
                         </div>
                         <div
                             class="hidden p-4 rounded-lg"
