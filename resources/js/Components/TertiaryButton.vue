4bf49b245d9fd9794bff5ff9f1566b652326c7f4
nazrinputra <putra_naz94@yahoo.com>
2024-01-27 15:24:02 +0800

    Fix buttons and layout

---
+++ resources/js/Components/TertiaryButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <button
+        class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md font-title bg-accent-orange hover:bg-light-orange focus:bg-light-orange active:bg-light-orange focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+    >
+        <slot />
+    </button>
+</template>
--- resources/js/Pages/Winners/Index.vue
+++ resources/js/Pages/Winners/Index.vue
@@ -1,7 +1,7 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import BreadcrumbItem from "@/Components/BreadcrumbItem.vue";
-import SecondaryButton from "@/Components/SecondaryButton.vue";
+import TertiaryButton from "@/Components/TertiaryButton.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { Head, Link, router } from "@inertiajs/vue3";
 
@@ -50,19 +50,71 @@ const claim = (winner) => {
                                 <div
                                     class="mt-4 space-x-4 sm:ml-16 sm:mt-0 sm:flex-none"
                                 >
-                                    <Link :href="route('winners.draw')">
-                                        <PrimaryButton> Demo </PrimaryButton>
-                                    </Link>
-                                    <Link :href="route('winners.createSingle')">
-                                        <PrimaryButton> Single </PrimaryButton>
-                                    </Link>
-                                    <Link
-                                        :href="route('winners.createMultiple')"
+                                    <PrimaryButton
+                                        id="dropdownDefaultButton"
+                                        data-dropdown-toggle="dropdown"
+                                        type="button"
                                     >
-                                        <PrimaryButton>
-                                            Multiple
-                                        </PrimaryButton>
-                                    </Link>
+                                        Lucky Draw
+                                        <svg
+                                            class="w-2.5 h-2.5 ms-3"
+                                            aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            fill="none"
+                                            viewBox="0 0 10 6"
+                                        >
+                                            <path
+                                                stroke="currentColor"
+                                                stroke-linecap="round"
+                                                stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="m1 1 4 4 4-4"
+                                            />
+                                        </svg>
+                                    </PrimaryButton>
+
+                                    <!-- Dropdown menu -->
+                                    <div
+                                        id="dropdown"
+                                        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                                    >
+                                        <ul
+                                            class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                            aria-labelledby="dropdownDefaultButton"
+                                        >
+                                            <li>
+                                                <Link
+                                                    :href="
+                                                        route('winners.draw')
+                                                    "
+                                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    >Demo</Link
+                                                >
+                                            </li>
+                                            <li>
+                                                <Link
+                                                    :href="
+                                                        route(
+                                                            'winners.createSingle'
+                                                        )
+                                                    "
+                                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    >Single</Link
+                                                >
+                                            </li>
+                                            <li>
+                                                <Link
+                                                    :href="
+                                                        route(
+                                                            'winners.createMultiple'
+                                                        )
+                                                    "
+                                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                                    >Multiple</Link
+                                                >
+                                            </li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                             <div class="mt-8 -mx-4 sm:-mx-0">
@@ -175,8 +227,27 @@ const claim = (winner) => {
                                                 class="px-3 py-4 text-sm text-gray-500 capitalize"
                                             >
                                                 <div
-                                                    class="grid grid-cols-1 gap-10 px-3 py-4 sm:gap-4 lg:grid-cols-2"
+                                                    class="flex flex-col justify-end gap-4 gap-y-2 md:flex-row"
                                                 >
+                                                    <span
+                                                        v-if="
+                                                            !winner.is_claimed
+                                                        "
+                                                        @click="claim(winner)"
+                                                    >
+                                                        <TertiaryButton>
+                                                            <svg
+                                                                class="w-4"
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                                viewBox="0 0 512 512"
+                                                                fill="currentColor"
+                                                            >
+                                                                <path
+                                                                    d="M190.5 68.8L225.3 128H224 152c-22.1 0-40-17.9-40-40s17.9-40 40-40h2.2c14.9 0 28.8 7.9 36.3 20.8zM64 88c0 14.4 3.5 28 9.6 40H32c-17.7 0-32 14.3-32 32v64c0 17.7 14.3 32 32 32H480c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H438.4c6.1-12 9.6-25.6 9.6-40c0-48.6-39.4-88-88-88h-2.2c-31.9 0-61.5 16.9-77.7 44.4L256 85.5l-24.1-41C215.7 16.9 186.1 0 154.2 0H152C103.4 0 64 39.4 64 88zm336 0c0 22.1-17.9 40-40 40H288h-1.3l34.8-59.2C329.1 55.9 342.9 48 357.8 48H360c22.1 0 40 17.9 40 40zM32 288V464c0 26.5 21.5 48 48 48H224V288H32zM288 512H432c26.5 0 48-21.5 48-48V288H288V512z"
+                                                                />
+                                                            </svg>
+                                                        </TertiaryButton>
+                                                    </span>
                                                     <Link
                                                         :href="
                                                             route(
@@ -198,25 +269,6 @@ const claim = (winner) => {
                                                             </svg>
                                                         </PrimaryButton>
                                                     </Link>
-                                                    <span
-                                                        v-if="
-                                                            !winner.is_claimed
-                                                        "
-                                                        @click="claim(winner)"
-                                                    >
-                                                        <SecondaryButton>
-                                                            <svg
-                                                                class="w-4"
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                viewBox="0 0 512 512"
-                                                                fill="currentColor"
-                                                            >
-                                                                <path
-                                                                    d="M190.5 68.8L225.3 128H224 152c-22.1 0-40-17.9-40-40s17.9-40 40-40h2.2c14.9 0 28.8 7.9 36.3 20.8zM64 88c0 14.4 3.5 28 9.6 40H32c-17.7 0-32 14.3-32 32v64c0 17.7 14.3 32 32 32H480c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H438.4c6.1-12 9.6-25.6 9.6-40c0-48.6-39.4-88-88-88h-2.2c-31.9 0-61.5 16.9-77.7 44.4L256 85.5l-24.1-41C215.7 16.9 186.1 0 154.2 0H152C103.4 0 64 39.4 64 88zm336 0c0 22.1-17.9 40-40 40H288h-1.3l34.8-59.2C329.1 55.9 342.9 48 357.8 48H360c22.1 0 40 17.9 40 40zM32 288V464c0 26.5 21.5 48 48 48H224V288H32zM288 512H432c26.5 0 48-21.5 48-48V288H288V512z"
-                                                                />
-                                                            </svg>
-                                                        </SecondaryButton>
-                                                    </span>
                                                 </div>
                                             </td>
                                         </tr>
