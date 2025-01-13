317c90313d9c88771d9d641dec24af914ffd0e2d
aiman <aiman.azhari@tm.com.my>
2025-01-13 10:37:46 +0800

    Update application logo and remove outdated countdown and attire components

--- resources/js/Components/ApplicationLogo.vue
+++ resources/js/Components/ApplicationLogo.vue
@@ -1,7 +1,7 @@
 <template>
     <img
         class="w-full h-auto sm:rounded-lg"
-        src="/images/Wording-01-2025.png"
+        src="/images/Wording-02-2025.png"
         alt="TM ONE"
     />
 </template>
